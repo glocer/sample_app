93929132b31f6ed6aa3545175696c69ddbe37224
Glocer Wang <glocer@gmail.com>
2011-08-05 23:19:55 -0400

    Removed default Rails page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  # Return a title on per-page basis.
+  def title
+    base_title = "Rails Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+  
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,12 @@ require 'spec_helper'
 describe PagesController do
   render_views
   
+  before(:each) do
+      #
+      # Define @base_title here
+      #
+  end
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +17,7 @@ describe PagesController do
     
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Rails App | Home")
+      response.should have_selector("title", :content => @base_title + " | Home")
     end
   end
 
@@ -22,7 +28,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Rails App | Contact")
+      response.should have_selector("title", :content => @base_title + " | Contact")
     end
   end
   
@@ -34,7 +40,22 @@ describe PagesController do
     
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Rails App | About")
+      response.should have_selector("title", :content => @base_title + " | About")
+    end
+    
+  end
+
+
+  
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title + " | Help")
     end
     
   end
